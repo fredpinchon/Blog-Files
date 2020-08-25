084b3d6b619cc04ff19e567c19eb18a1b4d1da94
fredpinchon <69602067+fredpinchon@users.noreply.github.com>
2020-08-24 22:32:04 -0700

    Update Order Counts Blog post.sql

--- Order Counts Blog post.sql
+++ Order Counts Blog post.sql
@@ -1,3 +1,5 @@
+// How to create rows to count properly line items
+// Customer Cohorts
 // Step 0: Show Orders details by Customers
 select "Customer Name","Order_Date","Order ID","Product Name","Quantity"
 from "INTERNAL"."PUBLIC"."SuperStore"
@@ -5,47 +7,47 @@ order by 1,2;
 
 // Step 1: Show multiple Orders by Customers
 select "Customer Name","Order_Date","Order ID", count(distinct "Order ID") as Orders,SUM(1) as "Line Items"
-
 from "INTERNAL"."PUBLIC"."SuperStore"
 group by 1,2,3
 order by 1,3;
 
 // Step 2: Show rank of Orders by Customers
-select "Customer Name","Order ID","Order_Date",RANK() OVER (PARTITION BY "Customer Name" ORDER by "Order_Date") as "Order Rank", count(distinct "Order ID") as Orders,SUM(1) as "Line Items"
+select "Customer Name","Order ID","Order_Date",DENSE_RANK() OVER (PARTITION BY "Customer Name" ORDER by "Order_Date") as "Order Rank", count(distinct "Order ID") as Orders,SUM(1) as "Line Items"
 from "INTERNAL"."PUBLIC"."SuperStore"
 group by 1,2,3
 order by 1,3;
+--- enhance table to include Rank and first order date for Customer Cohort
+CREATE OR REPLACE TABLE "INTERNAL"."PUBLIC"."SuperStore" AS
+select *,DENSE_RANK() OVER (PARTITION BY "Customer Name" ORDER by "Order_Date") as "Order Rank",First_Value("Order_Date") OVER (PARTITION BY "Customer Name" ORDER by "Order_Date") as "Customer Cohort"
+from "INTERNAL"."PUBLIC"."SuperStore";
 
-
-// Step 3: Show all customers who ordered a Products more than once
+// Product Sequence
+// Step 3: Show all customers who ordered a product more than once
 select "Customer Name","Product Name",SUM(1) as "Line Items"
 from "INTERNAL"."PUBLIC"."SuperStore"
 group by 1,2
 having "Line Items">1
-order by 1,2
-;
+order by 1,2;
 
 // Step 4: Show rank of Products by Customers
 select "Customer Name","Order_Date","Order ID","Product Name",RANK() OVER (PARTITION BY "Customer Name","Product Name" ORDER by "Order_Date") as "Product Rank",SUM(1) as "Line Items"
 from "INTERNAL"."PUBLIC"."SuperStore"
 group by 1,2,3,4
-order by 1,2
-;
+order by 1,2;
 
 // Step 5: Show rank of Products by Customers: check Suzanne
 select "Customer Name","Product Name","Order_Date",RANK() OVER (PARTITION BY "Customer Name","Product Name" ORDER by "Order_Date") as "Product Rank",SUM(1) as "Line Items"
 from "INTERNAL"."PUBLIC"."SuperStore"
 WHERE "Customer Name"='Suzanne McNair'
 group by 1,2,3
-order by 1,2
-;
+order by 1,2;
 
 // Step 6: Show rank of Products by Customers with Quantities: WRONG!
 select "Customer Name","Order_Date","Order ID","Product Name",RANK() OVER (PARTITION BY "Customer Name","Product Name" ORDER by "Order_Date") as "Product Rank",SUM(1) as "Line Items",SUM("Quantity")
 from "INTERNAL"."PUBLIC"."SuperStore"
+WHERE "Customer Name"='Suzanne McNair'
 group by 1,2,3,4
-order by 1,2
-;
+order by 1,4;
 
 select avg("Quantity") from "INTERNAL"."PUBLIC"."SuperStore";
 
@@ -76,8 +78,7 @@ SELECT SOURCE.*,N."NUMBER" as "Item Count"
 FROM "INTERNAL"."PUBLIC"."SuperStore" AS SOURCE
     JOIN
    "INTERNAL"."PUBLIC"."NUMBERS" N 
-   ON N.Number <= SOURCE."Quantity"
-  ;
+   ON N.Number <= SOURCE."Quantity" ;
 
 // Step 9: Update line items to keep totals
 UPDATE "INTERNAL"."PUBLIC"."SuperStore_Products"
@@ -90,9 +91,8 @@ UPDATE "INTERNAL"."PUBLIC"."SuperStore_Products"
        
 // Step 10: 
 CREATE OR REPLACE TABLE "INTERNAL"."PUBLIC"."SuperStore_Products" AS
-select *,RANK() OVER (PARTITION BY "Customer Name","Product Name" ORDER by "Order_Date","NUMBER") as "Product Rank"
-from "INTERNAL"."PUBLIC"."SuperStore_Products"
-;
+select *,RANK() OVER (PARTITION BY "Customer Name","Product Name" ORDER by "Order_Date","Item Count") as "Product Rank"
+from "INTERNAL"."PUBLIC"."SuperStore_Products";
 
 // Step 11: Test Table Before
 select *,RANK() OVER (PARTITION BY "Customer Name","Product Name" ORDER by "Order_Date") as "Product Rank"
@@ -105,4 +105,3 @@ select *
 from "INTERNAL"."PUBLIC"."SuperStore_Products"
 WHERE "Customer Name"='Suzanne McNair' AND "Product Name"='Southworth Parchment Paper & Envelopes'
 ORDER BY "Customer Name","Product Name","Product Rank";
-
